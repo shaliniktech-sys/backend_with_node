7fdc1b40c134cb6d5ace260127f825d85ccf68b9
SHALINI <shalinik.tech@gmail.com>
2026-07-05 22:09:27 +0530

    Update 01_sync_file_operations.js

--- 04_file_handling/01_sync_file_operations.js
+++ 04_file_handling/01_sync_file_operations.js
@@ -1,11 +1,29 @@
-// Synchronous File Handling in Node.js
-
 const fs = require("fs");
 
-// Write data to a file
+// Create or overwrite a file
 fs.writeFileSync("./text_sync.txt", "Hello World");
 
-// Read data from a file
-const result = fs.readFileSync("./contacts.txt", "utf-8");
-
+// Read file contents
+const result = fs.readFileSync("./contact.txt", "utf-8");
 console.log(result);
+
+// Append data to the file
+fs.appendFileSync(
+    "./text_sync.txt",
+    `\n${new Date().toLocaleString()}\nHey there!\n`
+);
+
+// Copy the file
+fs.cpSync("./contact.txt", "./contact_copy.txt");
+
+// Delete the copied file
+fs.unlinkSync("./contact_copy.txt");
+
+// Get file information
+const stats = fs.statSync("./text_sync.txt");
+console.log(stats.isFile()); // Returns true if it's a file
+
+// Create nested directories
+fs.mkdirSync("./my-docss/a/b", {
+    recursive: true,
+});
